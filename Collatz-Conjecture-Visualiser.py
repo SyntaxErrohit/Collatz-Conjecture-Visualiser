dc0792bab42f7388e107dcd6e39cee69d0d40eee
rohit elamurugan <rohitelamurugan1607@gmail.com>
2023-03-10 21:45:07 +0530

    made changes

--- Collatz-Conjecture-Visualiser.py
+++ Collatz-Conjecture-Visualiser.py
@@ -1,13 +1,10 @@
 import matplotlib.pyplot as plt
 
-for i in range(1, 61):
+for i in range(1, 1001):
     x = i
     y = [x]
     while x != 1:
-        if x%2 == 1:
-            x = 3*x + 1
-        else:
-            x = x // 2
+        x = 3*x + 1 if x%2 else x//2
         y.append(x)
     y.append(1)
     plt.plot(y)
